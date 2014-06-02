4fd2001abfb731ee667c2e62ce71fca5113109b2
Genaro Garza <github.rf9@gishpuppy.com>
2014-06-02 10:14:17 -0500

    finished todo list uploading files exercise

--- public/file_upload.php
+++ public/file_upload.php
@@ -8,6 +8,32 @@
 
 var_dump($_FILES);
 
+		// Verify there were uploaded files and no errors
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+    	// Set the destination directory for uploads
+    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+    	// Grab the filename from the uploaded file by using basename
+    $filename = basename($_FILES['file1']['name']);
+    	// Create the saved filename using the file's original name and our upload directory
+    $saved_filename = $upload_dir . $filename;
+    	// Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+}
+
+		// Check if we saved a file
+if (isset($saved_filename)) {
+    	// If we did, show a link to the uploaded file
+    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+}
+
+
+
+
+
+
+
+
+
 ?>
 
 <h1>Upload File</h1>
@@ -24,3 +50,5 @@ var_dump($_FILES);
 
 </body>
 </html>
+
+
--- public/todo_list.php
+++ public/todo_list.php
@@ -1,4 +1,4 @@
-<?php
+<?
 
 function open_file($x_filename)
 {
@@ -24,45 +24,66 @@ function save_file($filename, $contents)
 //--------------------------------------
 
 // >>>>>>>CODE STARTS HERE<<<<<<<<<<<<<<<<
+
+
+//1. retrieves saved list txt file
 $filename = "list.txt";
+define('FILENAME', "list.txt");
 //gets contents of file
-$todo_string = open_file($filename);
+$todo_string = open_file(FILENAME);
 //converts file contents string to array
 $list_array = explode("\n", $todo_string);
 
+//2. checks if removing item from hyperlink via GET
 if (isset($_GET['index']))
 {
     //removes item from hyperlink
     unset($list_array[$_GET['index']]);
 }
 
+//3. checks if item was added via POST
 if (!empty($_POST))
 {
-    //adds new item from submit
+        //3a. adds new item from submit
     array_push($list_array, $_POST["NewItem"]);
 }
 
-// uploads file
+// 4. checks if file was uploaded and uploads file
 if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) 
 {
-    // Set the destination directory for uploads
-    $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+    
+        //4a. checks if uploaded file is text
+    if ($_FILES['file1']['type'] == 'text/plain') 
+        {
+               //4b. if file is text type
 
-    // Grab the filename from the uploaded file by using basename
-    $filename = basename($_FILES['file1']['name']);
+            // Set the destination directory for uploads
+            $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
 
-    // Create the saved filename using the file's original name and our upload directory
-    $saved_filename = $upload_dir . $filename;
+            // Grab the filename from the uploaded file by using basename
+            $filename_up = basename($_FILES['file1']['name']);
 
-    // Move the file from the temp location to our uploads directory
-    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+            // Create the saved filename using the file's original name and our upload directory
+            $saved_filename = $upload_dir . $filename_up;
 
-    $string_to_add = open_file($saved_filename);
-    $array_to_add = explode("\n", $string_to_add);
-    $list_array = array_merge($list_array, $array_to_add);
+            // Move the file from the temp location to our uploads directory
+            move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+
+            //4c. merges uploaded file with existing file
+            $string_to_add = open_file($saved_filename);
+            $array_to_add = explode("\n", $string_to_add);
+            $list_array = array_merge($list_array, $array_to_add);
+           
+        } else 
+            {
+                echo "File type must be TXT";
+                echo '<script type="text/javascript">alert("type must be txt"); </script>';
+            }  
+            
+              
 }
 
-//converts array to string for saving to file   
+//6. converts array to string for saving to file   
 $contents = implode("\n", $list_array);
 save_file($filename, $contents);
 
